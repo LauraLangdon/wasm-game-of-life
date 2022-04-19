afbea62eec2280d43c8878a18ffabe52adda4537
Laura Langdon <48335772+LauraLangdon@users.noreply.github.com>
2022-04-19 14:25:16 -0700

    feat: add tests

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,34 @@ impl Universe {
 
         self.cells = next;
     }
+
+// (    pub fn set_width(&mut self, width: u32) {
+//         self.width=width;
+//         self.cells= (0..width * self.height).map(|_i| Cell::Dead).collect();
+//     }
+
+//     pub fn set_height(&mut self, height: u32) {
+//         self.height = height;
+//         self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+//     })
 }
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        self.cells = (0..self.width * self.height).map(|_i| Cell::Dead).collect();
+
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+}
+
+
 
 impl fmt::Display for Universe {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
